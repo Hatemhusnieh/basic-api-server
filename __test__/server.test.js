ceb9f36d46d4d04dcd0310069f9ae14e0baa9cbf
Hatemhusnieh <hatemhtm7@gmail.com>
2021-06-30 01:10:52 +0300

    lab 03 unfinished

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -2,7 +2,7 @@
 const supertest = require('supertest');
 const {app} = require('../src/server');
 const mockRequest = supertest(app);
-const gameRouter = require('../src/routes/gamesRoutes');
+// const gameRouter = require('../src/routes/gamesRoutes');
 
 describe('API server', () => {
   it('right path', async () => {
@@ -20,25 +20,65 @@ describe('API server', () => {
     expect(response.status).toEqual(200);
   });
   
-  // it('correct way to get game by id request', async () => {
-  //   await mockRequest.post('/api/v1/games').
-  //     send({
-  //       name: 'pes',
-  //       rate: 6.5,
-  //       genre: 'sport',
-  //     });
-  //   const response = await mockRequest.get(`/api/v1/games${id}`);
-  //   expect(response.status).toEqual(200);
-  // });
-  // it('correct way to create a game', async (req, res) => {
-  //   req.body = {
-  //     name: 'the witcher',
-  //     rate: 9.4,
-  //     genre: 'adventure',
-  //   };
-  //   const response = await mockRequest.post('/games/api/v1');
-  //   expect(response.status).toEqual(200);
-  // });
+  it('correct way to get game by id request', async () => {
+    const game = await mockRequest.post('/api/v1/games').send({
+      name: 'pes',
+      genre: 'sport',
+    });
+    const response = await mockRequest.get(`/api/v1/games/${game.id}`);
+    expect(response.status).toEqual(200);
+  });
 
+  it('correct way to create a game', async () => {
+    const response = await mockRequest.post('/api/v1/games').send({
+      name: 'pes',
+      rate: '6.5',
+      genre: 'sport',
+    });
+    expect(response.status).toEqual(200);
+  });
 
+  it('correct way to update a game', async () => {
+    const response = await mockRequest.put(`/api/v1/games/:id`).send({name: 'witcher', genre: 'action'} );
+    expect(response.status).toEqual(200); 
+  });
+
+  it('correct way to delete a game', async () => {
+    const response = await mockRequest.delete(`/api/v1/games/:id`).send({name: 'witcher', genre: 'action'} );
+    expect(response.status).toEqual(200); 
+  });
+
+  // player tests
+
+  it('correct way to get all players request', async () => {
+    const response = await mockRequest.get('/api/v1/players');
+    expect(response.status).toEqual(200);
+  });
+  
+  it('correct way to get player by id request', async () => {
+    const game = await mockRequest.post('/api/v1/players').send({
+      name: 'pes',
+      genre: 'sport',
+    });
+    const response = await mockRequest.get(`/api/v1/players/${game.id}`);
+    expect(response.status).toEqual(200);
+  });
+
+  it('correct way to create a player', async () => {
+    const response = await mockRequest.post('/api/v1/players').send({
+      name: 'hatem',
+      games: 'pes',
+    });
+    expect(response.status).toEqual(200);
+  });
+
+  it('correct way to update a player', async () => {
+    const response = await mockRequest.put(`/api/v1/players/:id`).send({name: 'hatem', games: 'pes'});
+    expect(response.status).toEqual(200); 
+  });
+
+  it('correct way to delete a player', async () => {
+    const response = await mockRequest.delete(`/api/v1/players/:id`).send({name: 'hatem', games: 'pes'});
+    expect(response.status).toEqual(200); 
+  });
 });
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const errHandler = require('./error-handlers/500');
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use('/api/v1/games', gamesRouter);
 app.use('/api/v1/players', playersRouter);
 
